2f797cc9fe1c07657b25d01f221c7ca29ea55ed7
sameemtypescript <sameem.amjad@zencloudtechnologies.com>
2026-02-24 08:59:47 +0500

    feat: complete copy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { shopifySans } from '@/app/font'
 export const metadata: Metadata = {
   title: "Cristopher",
   description: "Generated For Christopher",
+  icons: {
+    icon: "https://cdn.shopify.com/b/shopify-brochure2-assets/88ee7022e2749387148cb4098cc4f9fb.svg",
+  },
 };
 
 export default function RootLayout({
